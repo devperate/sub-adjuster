9dea99d92f5092324c41b626b4e52e4b826f441f
devperate <devperate@gmail.com>
2022-03-13 02:19:07 -0400

    code refactor, license update
    code refactor inside the adjust file and license file created and added into readme

--- adjust.js
+++ adjust.js
@@ -2,67 +2,67 @@ const fs = require('fs');
 const path = require('path');
 let iconv = require('iconv-lite');
 
-module.exports = (file, seconds)=>{
-	pRaw = fs.readFileSync(file);
-	raw = iconv.decode(pRaw, 'latin1');
-	//console.log(raw);
-	const ext = file.split('.')[file.split('.').length-1];
-	let fileName = '';
-	fileName += file.split('.').filter((chunk, index, array)=>{
-		return index < array.length-1;
-	});
-	const output = path.join(process.cwd(), `${fileName}.fixed.${ext}`);
-	
-	raw_array0 = raw.split('\n');
-	//console.log(raw_array0);
-	raw_array1 = raw_array0.map(function(val){
-		time = val.split(' --> ');
-		if(time.length > 1){
+module.exports = (file, seconds) => {
+	const raw_f = fs.readFileSync(file);
+	const text = iconv.decode(raw_f, 'latin1');
+	//console.log(text);
+	const output = path.join(process.cwd(), getOutputFileName(file));
+
+	const text_array = text.split('\n');
+	//console.log(text_array);
+	let text_array_fixed = text_array.map(function (line) {
+		time = line.split(' --> ');
+		if (time.length > 1) {
 			time[0] = makePlus(time[0], seconds);
 			//console.log(time[0]);
 			time[1] = makePlus(time[1], seconds);
 			//console.log(time[1]);
-			val = time[0] + ' --> ' + time[1];
-			//console.log(val);
+			line = time[0] + ' --> ' + time[1];
+			//console.log(line);
 		}
-		return val;
+		return line;
 	});
-	raw1 = '';
-	raw_array1.forEach(function(val) {
-		//console.log(val);
-		raw1 += val+'\n';
-		return;
-	});
-	//console.log(raw1);
 
-	fs.writeFile(output, raw1, 'utf8', (err) => {
+	const text_fixed = text_array_fixed.join('\n');
+
+	fs.writeFile(output, text_fixed, 'utf8', err => {
 		if (err) throw err;
-		console.log('\nIt\'s saved!', `${fileName}.fixed.${ext}`);
+		console.log("\nIt's saved!", `${fileName}.fixed.${ext}`);
 	});
+};
+
+function getOutputFileName(file) {
+	const ext = file.split('.')[file.split('.').length - 1];
+	let fileName = '';
+	fileName += file.split('.').filter((chunk, index, array) => {
+		return index < array.length - 1;
+	});
+
+	return `${fileName}.fixed.${ext}`;
 }
 
-function makePlus(time, adjust){
+function makePlus(time, adjust) {
 	//00:00:28,682 --> 00:00:30,682
 	time = time.split(':');
 	//console.log(parseFloat(time[2]) + adjust);
-	ms = time[2].split(',')[1];
+	const ms = time[2].split(',')[1];
 	time[2] = time[2].split(',')[0];
 	time[2] = parseInt(time[2]) + adjust;
 	time[1] = parseInt(time[1]);
 	time[0] = parseInt(time[0]);
 	//console.log('time[2] ' + time[2]);
-	if( time[2] >= 59 ){
+	if (time[2] >= 59) {
 		time[2] -= 60;
 		time[1] += 1;
-		
-		if( time[1] >= 59 ){
+
+		if (time[1] >= 59) {
 			time[1] -= 60;
 			time[0] += 1;
 		}
 	}
-	time[2] = ((''+time[2]).length == 1)?('0'+time[2]):(time[2])
-	time[1] = ((''+time[1]).length == 1)?('0'+time[1]):(time[1])
-	time[0] = ((''+time[0]).length == 1)?('0'+time[0]):(time[0])
+	time[2] = ('' + time[2]).length == 1 ? '0' + time[2] : time[2];
+	time[1] = ('' + time[1]).length == 1 ? '0' + time[1] : time[1];
+	time[0] = ('' + time[0]).length == 1 ? '0' + time[0] : time[0];
 	//console.log('the adjusted time is: ' + time[0]+':'+time[1]+':'+time[2]+','+ms);
-	return time[0]+':'+time[1]+':'+time[2]+','+ms;
+	return time[0] + ':' + time[1] + ':' + time[2] + ',' + ms;
 }
